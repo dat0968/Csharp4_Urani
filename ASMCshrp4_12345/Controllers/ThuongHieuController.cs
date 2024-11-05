ed1a07fa57bcec7852580d6fde6fd976d1e17a6a
pcbhoaanong <phancongbao11a3@gmail.com>
2024-11-06 00:04:12 +0700

    Thuong hieu Nhacc

---
+++ ASMCshrp4_12345/Controllers/ThuongHieuController.cs
@@ -0,0 +1,103 @@
+﻿using ASMCshrp4_12345.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASMCshrp4_12345.Controllers
+{
+    public class ThuongHieuController : Controller
+    {
+        private readonly Csharp4Context _context;
+        public ThuongHieuController(Csharp4Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(string searchString, string sortOrder, bool showDeleted = false)
+        {
+            ViewBag.ShowDeleted = showDeleted;
+
+            var thuonghieus = _context.Thuonghieus.AsQueryable();
+
+            if (!showDeleted)
+            {
+                thuonghieus = thuonghieus.Where(n => !n.IsDelete);
+            }
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                thuonghieus = thuonghieus.Where(n => n.TenThuongHieu.Contains(searchString) || n.MaThuongHieu.Contains(searchString));
+            }
+            if (sortOrder == "AZ")
+            {
+                thuonghieus = thuonghieus.OrderBy(n => n.TenThuongHieu);
+            }
+            else if (sortOrder == "ZA")
+            {
+                thuonghieus = thuonghieus.OrderByDescending(n => n.TenThuongHieu);
+            }
+
+            return View(thuonghieus.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Thuonghieu thuonghieu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(thuonghieu);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thuonghieu);
+        }
+        public IActionResult Edit(string id)
+        {
+            var thuonghieu = _context.Thuonghieus.Find(id);
+            if (thuonghieu == null)
+            {
+                return NotFound();
+            }
+            return View(thuonghieu);
+        }
+        [HttpPost]
+        public IActionResult Edit(string id, Thuonghieu thuonghieu)
+        {
+            if (id!=thuonghieu.MaThuongHieu)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(thuonghieu);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thuonghieu);
+        }
+        public IActionResult Delete(string id)
+        {
+            var thuonghieu = _context.Thuonghieus.Find(id);
+            if (thuonghieu == null)
+            {
+                return NotFound();
+            }
+            return View(thuonghieu);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var thuonghieu = _context.Thuonghieus.Find(id);
+            if (thuonghieu != null)
+            {
+                thuonghieu.IsDelete = true;
+                _context.Update(thuonghieu);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
--- ASMCshrp4_12345/obj/Debug/net8.0/ASMCshrp4_12345.AssemblyInfo.cs
+++ ASMCshrp4_12345/obj/Debug/net8.0/ASMCshrp4_12345.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASMCshrp4_12345")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6131f7445c95f66a5733f6f5aeaa514454a449a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099ce1e09deb232292dc34e85bec5f192e692b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASMCshrp4_12345")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASMCshrp4_12345")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
