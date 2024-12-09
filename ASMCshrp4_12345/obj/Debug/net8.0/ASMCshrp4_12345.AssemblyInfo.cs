b967dd801fa1f165af2b1194e54af5c2c8e0b3fb
Saber01a <hoangnthpk03650@gmail.com>
2024-12-09 17:52:21 +0700

    fix table va don vi tien VN

--- ASMCshrp4_12345/obj/Debug/net8.0/ASMCshrp4_12345.AssemblyInfo.cs
+++ ASMCshrp4_12345/obj/Debug/net8.0/ASMCshrp4_12345.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASMCshrp4_12345")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30dd02650fb0d6391e762f685608a2291c6e48d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a46c77c7a53f3af5d4f25c8f4612fd85ef445e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASMCshrp4_12345")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASMCshrp4_12345")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
